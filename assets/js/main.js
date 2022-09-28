b1fb1386e3e01bd65c6752b0efeddfb2dc705a17
Thiago Alves <thiago.vmatos@hotmail.com>
2022-09-28 17:22:10 -0300

    Testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,24 @@ function activeWork() {
 }
 
 linkwork.forEach(i => i.addEventListener('click', activeWork))
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48
+    }
+  }
+})
