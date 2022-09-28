f1e3a80b9663ef2fa6899ee25c3f9f6b52add500
Thiago Alves <thiago.vmatos@hotmail.com>
2022-09-28 17:20:03 -0300

    Work

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,23 @@ modalClose.forEach(mc => {
     })
   })
 })
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+    target: '.work__card'
+  },
+  animation: {
+    duration: 300
+  }
+})
+
+/* Link active work */
+const linkwork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+  linkwork.forEach(i => i.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkwork.forEach(i => i.addEventListener('click', activeWork))
